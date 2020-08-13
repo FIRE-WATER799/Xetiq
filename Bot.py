9b027b2223b9420b91398cc7e90da7ef24963ca0
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 16:08:36 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,5 +13,19 @@ async def on_connect():
 	if client._uptime is None:
 		print(f"Connected to Discord. Getting ready...")
 		print(f'-----------------------------')
+
+if(message.content=="How do I join Xetiq"):
+  join=""
+  join+="1. What server are you \n"
+  join+="-NAE \n"
+  join+="-NAW \n"
+  join+="-EU \n"
+  join+="-ASIA \n"
+  join+="-OCE \n \n"
+  join+="2. What do you want to join as \n \n"
+  join+=""
+  join+=""
+  join+=""
+  ctx.send(join)
 		
 client.run("NzQzNTUyNDcyMzQ5NTQwNDUy.XzWVMw.6CpqvEJIAA7LNTbgNw6MtVYOzVw")
