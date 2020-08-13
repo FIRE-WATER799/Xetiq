f677e8c9f8f3b9ca087241ee96af40c8e3cf8978
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 14:47:35 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,14 @@
+import discord, os
+from discord.ext import commands
 
+client = commands.Bot(command_prefix=x!, pm_help=True, owner_id=702954010008748174, case_insensitive=True)
+
+client.remove_command("help")
+
+@client.event
+async def on_connect():
+	if client._uptime is None:
+		print(f"Connected to Discord. Getting ready...")
+		print(f'-----------------------------')
+		
+client.run("NzQzNTUyNDcyMzQ5NTQwNDUy.XzWVMw.6CpqvEJIAA7LNTbgNw6MtVYOzVw")
