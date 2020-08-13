561715410737427a5695d5a6c30a03c638857967
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 18:21:29 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -32,6 +32,8 @@ async def How_do_I_join_Xetiq(ctx):
   join+="Content Creator: U need atleast 100+ subs to be a content creator \n \n"
   join+="GFX/VFX: U have to make good work and dm staff or owners ur work \n \n"
   join+="And thats how u Join Xetiq so try to join this clan and be apart with the members in the clan!"
-  await ctx.send(join)
+  embed=discord.Embed(color=0x0000, title="How to join", description=join)
+  embed.set_footer(text="Created by fire#7010") 
+  await ctx.send(embed=embed)
 		
 client.run("NzQzNTUyNDcyMzQ5NTQwNDUy.XzWVMw.6CpqvEJIAA7LNTbgNw6MtVYOzVw")
