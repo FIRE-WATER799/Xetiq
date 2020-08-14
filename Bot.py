5b0c9ccce5bd9997774bcce12195b71135762684
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 19:01:56 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ from discord import member
 from discord.ext.commands import Bot
 
 PREFIX="$"
-embed_toggle=1
+embed_toggle=0
 
 client = commands.Bot(command_prefix=PREFIX, pm_help=True, owner_id=702954010008748174, case_insensitive=True)
 
@@ -33,7 +33,7 @@ async def How_do_I_join_Xetiq(ctx):
   join+="Content Creator: U need atleast 100+ subs to be a content creator \n \n"
   join+="GFX/VFX: U have to make good work and dm staff or owners ur work \n \n"
   join+="And thats how u Join Xetiq so try to join this clan and be apart with the members in the clan!"
-  if({embed_toggle}==0):
+  if(embed_toggle==0):
     await ctx.send(join)
   if({embed_toggle}==1):
     join_embed=discord.Embed(color=0x0000, title="How to join", description=join)
