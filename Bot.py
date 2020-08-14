0b75784e9dec9d4cd18a42c6720ae2d3ff33d19d
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 20:39:17 -0500

    I

--- Bot.py
+++ Bot.py
@@ -57,5 +57,8 @@ async def help(ctx):
 
       await ctx.send(embed=help_embed)
     
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 		
 client.run(config["token"])
