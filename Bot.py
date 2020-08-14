33b56084a608cb65c811bec060f690591f19eb91
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-14 10:35:41 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -59,7 +59,7 @@ async def help(ctx):
     
 @client.command(hidden=True)
 async def Congratulations(ctx, *, arg=None):
-  if(ctx.message.author.id==702954010008748174 or 361143341901152267):
+  if(ctx.message.author.id==702954010008748174):
     if arg == None:
       if(config["embed_toggle"]==0):
         await ctx.send("Error: Please specify who you want to congratulate")
