04f9af9ea14c7e9024cb2f437d24cc4717654f94
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-13 20:42:39 -0500

    update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import time 
 import json
 
+config = json.load(open('config.json'))
+
 class ping(commands.Cog):
   
     def __init__(self, client):
@@ -11,8 +13,6 @@ class ping(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Loaded ping.py')
-        
-    config = json.load(open('config.json'))
     
     @commands.command(usage="Get's the bot's ping")
     async def ping(self, ctx):
@@ -21,9 +21,9 @@ class ping(commands.Cog):
       ping = (time.monotonic() - before) * 1000
       ping_content = (f":ping_pong:   |   {int(ping)}ms\n"
              f":timer:   |   {self.client.latency * 1000:.0f}ms")
-      if config["embed_toggle"]==0:
+      if(config["embed_toggle"]==0):
         await message.edit(ping_content)
-      if config["embed_toggle"]==1:
+      if(config["embed_toggle"]==1):
         ping_embed=discord.Embed(color=0x0000, title="ping", description=ping_content)
         await message.edit(embed=ping_embed)
         
